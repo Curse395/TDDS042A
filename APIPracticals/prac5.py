d403a694f3fc8fa697f10d9b1af77828c1a39728
User <user@example.com>
2026-07-28 08:49:47 +0530

    API nested class method with field validator

--- APIPracticals/prac5.py
+++ APIPracticals/prac5.py
@@ -4,14 +4,14 @@ from pydantic import BaseModel,Field
 app = FastAPI()
 
 class Address(BaseModel):
-    street: str
+    street: str=Field(min_length=1,max_length=60)
     city: str
-    postal_code: int=Field(min_length=1 , max_length=6)
+    postal_code: str=Field(min_length=1,max_length=6)
     
 class User(BaseModel):
     name: str
-    age: int
-    address: Address=Field(min_length=1,max_length=60)
+    age: int=Field(gt=18,lt=60)
+    address: Address
 
 @app.post("/users/")
 async def create_user(user: User):
